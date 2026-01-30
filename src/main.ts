7a9d8805bc520679e4cabd65e48415a8bfad3700
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2026-01-30 15:22:02 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://trazabilidad-frontend-production-1a88.up.railway.app'],
     credentials: true, // si usás cookies o auth con headers
   });
   await app.listen(process.env.PORT ?? 3000);

7e086a4b790ffa2873234c1e9b224b0cce97074b
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2026-02-02 13:44:20 -0300

    Trazabilidad

--- src/modules/orden-produccion/entities/orden-consumo.entity.ts
+++ src/modules/orden-produccion/entities/orden-consumo.entity.ts
@@ -1,16 +1,18 @@
-import { Entity, Column, ManyToOne } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { TenantBaseEntity } from '../../../common/entities/tenant-base.entity';
 import { OrdenIngrediente } from './orden-ingrediente.entity';
 import { LoteMP } from '../../lotes/entities/lote-mp.entity';
 
 @Entity('orden_consumos')
 export class OrdenConsumo extends TenantBaseEntity {
-  @ManyToOne(() => OrdenIngrediente, (ing) => ing.consumos)
+  @ManyToOne(() => OrdenIngrediente, (ing) => ing.consumos, { nullable: false })
+  @JoinColumn({ name: 'ingrediente_id' })
   ingrediente: OrdenIngrediente;
 
-  @ManyToOne(() => LoteMP, { eager: true })
+  @ManyToOne(() => LoteMP, { eager: true, nullable: false })
+  @JoinColumn({ name: 'lote_id' })
   lote: LoteMP;
 
-  @Column('decimal')
+  @Column('decimal', { name: 'cantidad_kg' })
   cantidadKg: number;
 }
